e871b76c178a97358ab0a24bb807f55f4473d546
NenyeIkpa <nenyeikpa@gmail.com>
2023-06-15 23:25:23 +0000

    C program that prints a string, followed by a new line, to the standard error.

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+/**
+ *main - entry point
+ *no arguments
+ *
+ *Description: prints a string using write function.
+ *Return: main() has a return type of int. returns 1.
+ */
+int main(void) {
+    char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, message, 59);
+    return 1;
+}
