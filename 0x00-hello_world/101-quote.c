c30fad4eb0af2289af6a143b5181490cb0cd72ca
NenyeIkpa <nenyeikpa@gmail.com>
2023-06-15 23:27:19 +0000

    code indentation as Betty says

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,8 +6,9 @@
  *Description: prints a string using write function.
  *Return: main() has a return type of int. returns 1.
  */
-int main(void) {
+int main(void) 
+{
     char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
     write(2, message, 59);
-    return 1;
+    return (1);
 }
