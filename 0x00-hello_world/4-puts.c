0459f4eca31ab8d5226b44a3d8f7ae1b955aca91
NenyeIkpa <nenyeikpa@gmail.com>
2023-06-15 21:39:20 +0000

    updated comment

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 /*
- *main - Prints "Programming is like building a multilingual puzzle"
+ *main - entry point
+ *
+ *Desciption: Prints a string using puts.
+ *The string includes the an open double-quotes mark.
+ *Used the escape character \ to ensure quote is displayed.
  *Return: returns an int 0
  */
 int main(void)
