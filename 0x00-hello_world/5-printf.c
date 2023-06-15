358eab2b2fc4e8f0bb8ab75087d4ee74310ede02
NenyeIkpa <nenyeikpa@gmail.com>
2023-06-15 21:51:35 +0000

    C program that a string, followed by a new line.

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ *main - entry point
+ *no arguments
+ *Description: declared a variable whatToPrint
+ *of char type that holds a string. Using the
+ *prinf function to print out the content of
+ *the variable.
+ *Return: main() has a return type of int. returns 0.
+ */
+int main(void)
+{
+char whatToPrint;
+whatToPrint = "with proper grammar, but the outcome is a piece of art,";
+printf("%s\n", whatToPrint );
+return (0);
+}
