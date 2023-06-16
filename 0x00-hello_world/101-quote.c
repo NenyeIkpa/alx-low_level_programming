c36b760dde36c93a64e8148fe7e53e7c7d2935e1
NenyeIkpa <nenyeikpa@gmail.com>
2023-06-16 12:51:25 +0000

    C program that prints a string, followed by a new line, to the standard error using write().

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+#include <unistd.h>
+
 /**
  *main - entry point
  *no arguments
  *
- *Description: prints a string using fputs function.
+ *Description: prints a string using write function.
  *Return: main() has a return type of int. returns 1.
  */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			stderr);
+	char *message =
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 59);
 	return (1);
 }
