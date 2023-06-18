dbb9f1ba1b42dd5bf75c6c65e060520c73eb9efa
NenyeIkpa <nenyeikpa@gmail.com>
2023-06-18 19:03:28 +0000

    program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -19,12 +19,12 @@ int main(void)
 	{
 		for (y = x + 1; y < 10; y++)
 		{
-			for (z = x + 2; z < 10; z++)
+			for (z = y + 1; z < 10; z++)
 			{
 				putchar('0' + x);
 				putchar('0' + y);
 				putchar('0' + z);
-				if (x < 7 || y < 8 || z-y != 0 )
+				if (x < 7 || y < 8 || z < 9)
 				{
 					putchar(',');
 					putchar(' ');
