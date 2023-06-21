375a2ba33ba732672cdfe4129e08e971eaba92f8
NenyeIkpa <nenyeikpa@gmail.com>
2023-06-21 22:09:28 +0000

    function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,60 +12,60 @@
 
 void print_times_table(int n)
 {
-        int i = 0, j, result, c, d, e;
+	int i = 0, j, result, c, d, e;
 
-       if ( n >= 0 && n <= 15)
-       {
-       	while (i <= n)
-        {
-                j = 0;
-                while (j <= n)
-                {
-                        result = i * j;
-			if (result > 99)
+	if (n >= 0 && n <= 15)
+	{
+		while (i <= n)
+		{
+			j = 0;
+			while (j <= n)
 			{
-				c = result / 100;
-				d = (result - 100) / 10;
-				e = (result - 100) % 10;
-			}
-			else
-			{
-				c = result / 10;
-                        	d = result % 10;
-			}
-                        if ((c != 0 && d != 0) || (c > 0 && d == 0))
-                        {
-				if (result < 100)
+				result = i * j;
+				if (result > 99)
 				{
+					c = result / 100;
+					d = (result - 100) / 10;
+					e = (result - 100) % 10;
+				}
+				else
+				{
+					c = result / 10;
+					d = result % 10;
+				}
+				if ((c != 0 && d != 0) || (c > 0 && d == 0))
+				{
+					if (result < 100)
+					{
+						_putchar(' ');
+					}
+					_putchar('0' + c);
+					_putchar(d + '0');
+					if (result > 99)
+						_putchar(e + '0');
+				}
+				else
+				{
+					if (j == 0)
+					{
+						_putchar(d + '0');
+					}
+					else
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(d + '0');
+					}
+				}
+				if (j < n)
+				{
+					_putchar(',');
 					_putchar(' ');
 				}
-                                _putchar('0' + c);
-                                _putchar(d + '0');
-				if (result > 99)
-				_putchar(e + '0');;
-                        }
-                        else
-                        {
-                                if (j == 0)
-                                {
-                                        _putchar(d + '0');
-                                }
-                                else
-                                {
-					_putchar(' ');
-					_putchar(' ');
-                                        _putchar(d + '0');
-                                }
-                        }
-                        if (j < n)
-                        {
-                                _putchar(',');
-                                _putchar(' ');
-                        }
-                        j++;
-                }
-                _putchar('\n');
-                i++;
-        }
-       }
+				j++;
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
 }
