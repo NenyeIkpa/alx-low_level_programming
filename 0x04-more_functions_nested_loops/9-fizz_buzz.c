68b22eee9431c6cf4ca110aa52350c371d19eef9
NenyeIkpa <nenyeikpa@gmail.com>
2023-06-22 23:08:54 +0000

    program that prints the numbers from 1 to 100, Fizz, Buzz and FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+
+/**
+ * print_fizz - prints Fizz
+ *
+ * Description: prints the word, Fizz
+ */
+
+void print_fizz(void)
+{
+	int j = 0;
+	char fizz[4] = "Fizz";
+
+	while (fizz[j] != '\0')
+	{
+		printf("%c", fizz[j]);
+		j++;
+	}
+}
+
+/**
+ * print_buzz - prints Buzz
+ *
+ * Description: prints the word, Buzz
+ */
+
+void print_buzz(void)
+{
+	int k = 0;
+	char buzz[4] = "Buzz";
+
+	while (buzz[k] != '\0')
+	{
+		printf("%c", buzz[k]);
+		k++;
+	}
+	putchar(' ');
+}
+
+/**
+ * fizz_buzz - prints numbers, Fizz, Buzz and FizzBuzz
+ *
+ * Description: prints Fizz for multiples of 3,
+ * Buzz for multiple of 5 and FizBuzz for both
+ */
+
+void fizz_buzz(void)
+{
+	int j;
+	char fizz[4] = "Fizz";
+	char buzz[4] = "Buzz";
+
+	for (j = 1; j <= 100; j++)
+	{
+		if (j % 3 == 0 && j % 5 == 0)
+		{
+			print_fizz();
+			print_buzz();
+		}
+		else if (j % 3 == 0)
+		{
+			print_fizz();
+			putchar(' ');
+		}
+		else if (j % 5 == 0)
+			print_buzz();
+		else
+			printf("%d ", j);
+	}
+	putchar('\n');
+}
+
+/**
+ * main - calls function fizz_buzz
+ *
+ * Description: checks fizz_buzz code
+ *
+ * Return: type of int
+ */
+
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
+
