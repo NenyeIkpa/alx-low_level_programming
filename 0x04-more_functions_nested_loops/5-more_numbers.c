881b88c698f665c6eab5062a6d256df4b772e378
NenyeIkpa <nenyeikpa@gmail.com>
2023-06-22 20:50:47 +0000

    removed print_double_digits function

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,40 +8,25 @@
 
 void more_numbers(void)
 {
-	int max = 0, num;
+	int max = 0, num, first, second;
 
 	while (max < 10)
 	{
 		num = 0;
 		while (num < 15)
 		{
-			if ( num < 10)
+			if (num < 10)
 				_putchar('0' + num);
 			else
-				print_double_digits(num);
+			{
+				first = num / 10;
+				second = num % 10;
+				_putchar('0' + first);
+				_putchar('0' + second);
+			}
 			num++;
 		}
 		_putchar('\n');
 		max++;
 	}
-}
-
-/**
- * print_double_digits - prints double digits
- *
- * Description: prints double digits using
- * _putchar
- *
- * Return: 0
- */
-
-int print_double_digits(int num)
-{
-       	int first, second;
-
-	first = num / 10;
-	second = num % 10;
-	_putchar('0' + first);
-	_putchar('0' + second);
-	return (0);
 }
