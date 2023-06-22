98598c1a8e137a8cc1d97082d8148c83e7c0fb45
NenyeIkpa <nenyeikpa@gmail.com>
2023-06-22 21:53:46 +0000

    function that draws a diagonal line on the termi

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - prints \
+ *
+ * Description: prints \ n numbers of times diagonally
+ * @n: input of type int
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			j = 0;
+			while (j < i)
+			{
+				_putchar(' ');
+				j++;
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
+
