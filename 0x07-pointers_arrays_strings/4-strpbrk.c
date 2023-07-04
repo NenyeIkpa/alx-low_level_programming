499ac759ba6096981c7ae10aab3207e2e2ecafdc
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-04 10:37:26 +0000

    function that searches a string for any of a set of bytes.

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,27 +10,23 @@
  * Return: char *
  */
 
-/*
- char *_strpbrk(char *s, char *accept)
+char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
-	int a_length = strlen(accept);
 
-	if (s == NULL || accept == NULL)
-		return (NULL);
-	for (i = 0; s[i] != '\0'; i++)
+	for (i = 0; *(s + i) != '\0'; s++)
 	{
-		for (j = 0; j != a_length; j++)
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			if (s[i] == accept[j])
-				return (s + i);
+			if (*(s + i) == *(accept + j))
+				return (s);
 		}
 	}
 	return (NULL);
 }
-*/
 
-char *_strpbrk(char *s, char *accept)
+/*
+ * char *_strpbrk(char *s, char *accept)
 {
 	char *a;
 
@@ -47,3 +43,4 @@ char *_strpbrk(char *s, char *accept)
 	}
 	return (NULL);
 }
+*/
