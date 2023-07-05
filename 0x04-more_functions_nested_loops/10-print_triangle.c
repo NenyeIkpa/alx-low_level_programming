20fdab706915e296e9a9c98c252b093a5f9be0c6
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-05 09:05:15 +0000

    advanced try

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -14,20 +14,12 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (i = 1; i <= size; i++)
+		for (i = size; i < 0; i--)
 		{
-			j = size;
-			while (j > 0)
-			{
-				_putchar(' ');
-				j--;
-			}
-			_putchar('#');
+			for (j = 0; j < i; j++)
+				_putchar('#');
 			_putchar('\n');
 		}
 	}
-	else
-	{
-		_putchar('\n');
-	}
+	_putchar('\n');
 }
