50ccb0b437c860ce965d4e52c250af9b4b0db297
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-06 04:15:06 +0000

    function that returns 1 if the input integer is a prime number, otherwise return 0

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: given number
+ *
+ * Return: int
+ */
+
+int _sqrt_recursion(int n)
+{
+
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,19 @@
+/**
+ * is_prime_number - checks if input is prime
+ *
+ * @n: given input
+ *
+ * Return: int
+ */
+
+int is_prime_number(int n)
+{
+	if ( n <= 1)
+		return (0);
+	else if (n == 2 || n == 3 || n == 5 || n == 7)
+		return (1);
+	else if (n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0)
+		return (0);
+	else
+		return (1);
+}
