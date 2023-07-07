4e51d8a747c79be4ba7e0716f2608f67322bb868
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-07 11:27:53 +0000

    function that returns 1 if a string is a palindrome and 0 if not

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,25 +1,39 @@
 #include <string.h>
 
 /**
- * is_palindrome - checks if string is a palindrome
+ * is_palindrome_recursive - checks if string is a palindrome
  *
  * @s: given string
+ * @head: first index of string
+ * @tail: last index of string
+ *
+ * Return: int
+ */
+
+int is_palindrome_recursive(char *s, int head, int tail)
+{
+	if (head > tail)
+		return(1);
+	if (s[head] == s[tail])
+		return (is_palindrome_recursive(s, head + 1, tail - 1));
+	return (0);
+}
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ *
+ * @s: input string
  *
  * Return: int
  */
 
 int is_palindrome(char *s)
 {
-	int length, isSame = 0;
+	int length;
 
 	length = strlen(s);
-	if (length <= 1)
+
+	if (length == 0 || length == 1)
 		return (1);
-	if (*s == *(s + length - 1))
-	{
-		isSame = 1;
-		return (is_palindrome(s + 1));
-	}
-	return (isSame);
+	return (is_palindrome_recursive(s, 0, length - 1));
 }
-			
