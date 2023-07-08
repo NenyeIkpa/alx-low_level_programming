6f343b3d6fa6c0e35e531c82e3c1321173209adb
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-08 18:25:34 +0000

    applied DRY principle

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -2,16 +2,40 @@
 #include <stdlib.h>
 
 /**
+ * get_divisor - returns the divisor for a given number
+ *
+ * @value: given number
+ *
+ * @Return: int
+ */
+
+int get_divisor(int value)
+{
+	int divisor;
+
+	if (value > 24)
+		divisor = 24;
+	if (value >= 10 && value <= 24)
+		divisor = 10;
+	if (value >= 5 && value <= 9)
+		divisor = 5;
+	if (value >= 2 && value <= 4)
+		divisor = 2;
+	return (divisor);
+}
+
+/**
  * get_change - prints the minimum number of coins
  * to make change for an amount of money
  *
  * @value: given int
  * @cc: count of type int
+ * @divisor: denominator of type int
  *
  * Return: int
  */
 
-int get_change(int value, int cc)
+int get_change(int value, int cc, int divisor)
 {
 	if (value < 0)
 		return (0);
@@ -20,35 +44,11 @@ int get_change(int value, int cc)
 		cc++;
 		return (cc);
 	}
-	if (value >= 2 && value < 5)
-	{
-		cc = cc + value / 2;
-		if (value / 2 == 0)
-			return (cc);
-		value = value % 2;
-		return (get_change(value, cc));
-	}
-	if (value >= 5 && value < 10)
-	{
-		cc = cc + value / 5;
-		if (value / 5 == 0)
-			return (cc);
-		value = value % 5;
-		return (get_change(value, cc));
-	}
-	if (value >= 10 && value < 25)
-	{
-		cc = cc + value / 10;
-		if (value / 10 == 0)
-			return (cc);
-		value = value % 10;
-		return (get_change(value, cc));
-	}
-	cc = cc + value / 25;
-	if (value / 25 == 0)
+	cc = cc + value / divisor;
+	if (value / divisor == 0)
 		return (cc);
-	value = value % 25;
-	return (get_change(value, cc));
+	value = value % 2;
+	return (get_change(value, cc, get_divisor(value)));
 }
 
 /**
@@ -62,11 +62,14 @@ int get_change(int value, int cc)
 
 int main(int argc, char *argv[])
 {
+	int value;
+
 	if (argc < 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	printf("%d\n", get_change(atoi(argv[1]), 0));
+	value = atoi(argv[1]);
+	printf("%d\n", get_change(value, 0, get_divisor(value)));
 	return (0);
 }
