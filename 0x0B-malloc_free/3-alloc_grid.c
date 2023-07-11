392de9c080258b86ee921671e89fb6d38a4141fb
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-11 14:30:45 +0000

    added free fxn in case of fail

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -18,7 +18,9 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 		int_arr[i] = calloc(width, sizeof(int));
 	if (int_arr == NULL)
+	{
 		free(int_arr);
 		return (NULL);
+	}
 	return (int_arr);
 }
