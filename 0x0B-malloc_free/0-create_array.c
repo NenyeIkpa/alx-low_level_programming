837d876b7c3fd59a4114aad0f1db11dd550c6672
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-11 09:36:09 +0000

    handled malloc return

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,6 +18,8 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 	char_array = malloc(size * sizeof(char));
+	if (char_array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		char_array[i] = c;
 	return (char_array);
