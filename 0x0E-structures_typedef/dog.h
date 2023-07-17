4e5d45e7164cde688a010b37f5bfc507cfe2cb6c
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-17 22:15:01 +0000

    defined a struct type in c

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H_
+#define DOG_H_
+
+/**
+ * struct dog -  user defined data type of dog
+ *
+ * @name: dog name of type char
+ * @age: dog age of type int
+ * @owner: dog owner of type char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
