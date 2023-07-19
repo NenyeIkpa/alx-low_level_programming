5db58ea71410bf2b60391dd79edb39709350d642
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-19 08:48:46 +0000

    refactored  3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,7 +14,7 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	int_arr = malloc(height * sizeof(int *));
+	**int_arr = malloc(height * sizeof(int *));
 	if (int_arr == NULL)
 	{
 		return (NULL);
