641dd5f3897e3896f37fd4d12698bdadddcd27f2
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-28 03:09:03 +0000

    64-bit program in assembly that prints Hello, Holberton, followed by a new line

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,20 +1,17 @@
 ; Writes "Hello, Holberton" to the console using only system calls. Runs on 64-bit macOS only.
 ; To assemble and run: nasm -fmacho64 hello.asm && ld hello.o && ./a.out
 
-          global    start
+  global    _start
 
           section   .text
-_start:
-  mov rax, 1        ; write(
-  mov rdi, 1        ;   STDOUT_FILENO,
-  mov rsi, msg      ;   "Hello, Holberton!\n",
-  mov rdx, msglen   ;   sizeof("Hello, Holberton!\n")
-  syscall           ; );
+_start:   mov       rax, 1                  ; system call for write
+          mov       rdi, 1                  ; file handle 1 is stdout
+          mov       rsi, message            ; address of string to output
+          mov       rdx, 13                 ; number of bytes
+          syscall                           ; invoke operating system to do the write
+          mov       rax, 60                 ; system call for exit
+          xor       rdi, rdi                ; exit code 0
+          syscall                           ; invoke operating system to exit
 
-  mov rax, 60       ; exit(
-  mov rdi, 0        ;   EXIT_SUCCESS
-  syscall           ; );
-
-section .rodata
-  msg: db "Hello, Holberton!", 10
-  msglen: equ $ - msg
+          section   .data
+message:  db        "Hello, Holberton", 10      ; note the newline at the end
