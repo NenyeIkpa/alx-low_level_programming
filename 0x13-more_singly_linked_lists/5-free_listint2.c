3f8cdd6f4e5e1682058d62d6d6276affbb2d70b3
NenyeIkpa <nenyeikpa@gmail.com>
2023-07-31 18:07:51 +0000

    update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,13 +10,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp, *curr;
 
-	if (head != NULL)
+	if (*head != NULL)
+	{
 		curr = *head;
+		*head = NULL;
+	}
 	while (curr != NULL)
 	{
 		temp = curr;
 		curr = curr->next;
 		free(temp);
 	}
-	*head = NULL;
 }
