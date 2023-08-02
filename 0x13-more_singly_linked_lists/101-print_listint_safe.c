3783f695fe092d3e65fb91f6129fc5eaf30e7aae
NenyeIkpa <nenyeikpa@gmail.com>
2023-08-02 15:22:59 +0000

    task 12

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -12,19 +12,19 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	int count = 0;
+	listint_t *temp;
 
 	if (head == NULL)
 		exit(98);
-	while (head != NULL)
+	printf("[%p] %d\n", (void *)head, head->n);
+	count = 1;
+	temp = head->next;
+	while (temp != NULL && temp != head)
 	{
+		printf("[%p] %d\n", (void *)temp, temp->n);
 		count++;
-		printf("[%p] %d\n", (void *)head, head->n);
-		if (head <= head->next)
-		{
-			printf("-> [%p] %d\n", (void *)head->next, head->next->n);
-			exit(98);
-		}
-		head = head->next;
+		temp = temp->next;
 	}
+	exit (98);
 	return (count);
 }
