2475bff1b8ad984eac68a8f8fa7444eb05fc51f7
NenyeIkpa <nenyeikpa@gmail.com>
2023-08-03 01:37:21 +0000

    function that prints a listint_t linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,9 +1,14 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a list
+ * print_listint_safe - prints a list. detects loop in list
+ * using Floyd's cycle-finding algorithm.
  *
  * @head: given linked list
+ *
+ * Description: using two pointers(Floyd's cycle finder) loop through
+ * list and locate loop. loop through list again and print until loop
+ * point is reached again. print the loop and end program.
  *
  * Return: number of nodes in list
  *
@@ -12,21 +17,48 @@
 size_t print_listint_safe(const listint_t *head)
 {
 	size_t count = 0;
-	const listint_t *temp, *hold = NULL;
+	const listint_t *tortoise, *hare, *loop_in_list;
+	int loop_reached = 0;
 
-	temp = head;
-	while (temp != NULL)
+	if (head == NULL)
+		exit(98);
+	tortoise = head;
+	hare = head;
+	while (hare != NULL && hare->next != NULL)
 	{
-		printf("[%p] %d\n", (void *)temp, temp->n);
-		count++;
-
-		if (temp <= temp->next && hold != NULL)
+		tortoise = tortoise->next;
+		hare = hare->next->next;
+		if (hare == tortoise)
 		{
-			printf("-> [%p] %d\n", (void *)temp->next, temp->next->n);
-			exit(98);
+			tortoise = head;
+			while (tortoise != hare)
+			{
+				tortoise = tortoise->next;
+				hare = hare->next;
+			}
+			loop_in_list = hare;
+			break;
 		}
-		hold = temp;
-		temp = temp->next;
+	}
+	while (head != NULL)
+	{
+		printf("[%p] %d\n", (void *)head, head->n);
+		count++;
+		head = head->next;
+
+		if (head == loop_in_list)
+		{
+			if (loop_reached == 0)
+			{
+				loop_reached++;
+				continue;
+			}
+			else
+			{
+				printf("-> [%p] %d\n", (void *)head, head->n);
+				break;
+			}
+		}
 	}
 	return (count);
 }
