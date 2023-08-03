9e1f10a20b59a46655d1d00cf902354ba7add776
NenyeIkpa <nenyeikpa@gmail.com>
2023-08-03 14:45:36 +0000

    task 13 update

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+size_t free_listint_safe(listint_t **h);
+
 /**
  * free_listint_safe - frees a list
  *
@@ -17,8 +19,25 @@ size_t free_listint_safe(listint_t **h)
 	if (h == NULL || *h == NULL)
 		return (0);
 	loop = loop_in_list(*h);
+	if (loop == *h)
+	{
+		while ((*h)->next != loop)
+		{
+			temp = *h;
+			count++;
+			*h = (*h)->next;
+			free((void *)temp);
+		}
+		free(*h);
+		*h = NULL;
+		return (++count);
+	}
 	while (*h != NULL)
 	{
+		temp = *h;
+		count++;
+		*h = (*h)->next;
+		free((void *)temp);
 		if (*h == loop)
 		{
 			if (loop_reached == 0)
@@ -28,17 +47,11 @@ size_t free_listint_safe(listint_t **h)
 			}
 			else
 			{
-				temp = *h;
-				count++;
+				free(*h);
 				*h = NULL;
-				free((void *)temp);
 				break;
 			}
 		}
-		temp = *h;
-		count++;
-		*h = (*h)->next;
-		free((void *)temp);
 	}
 	return (count);
 }
