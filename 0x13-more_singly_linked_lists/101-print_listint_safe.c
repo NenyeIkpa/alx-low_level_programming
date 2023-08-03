45ab2d243ea6e45eccb37ca3c700dc37cd68a9a8
NenyeIkpa <nenyeikpa@gmail.com>
2023-08-03 02:03:06 +0000

    separated task into two functions

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,24 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint_safe - prints a list. detects loop in list
- * using Floyd's cycle-finding algorithm.
+ * loop_in_list - return the loop in a given linked list
  *
- * @head: given linked list
+ * @head: head pointer of given list
  *
- * Description: using two pointers(Floyd's cycle finder) loop through
- * list and locate loop. loop through list again and print until loop
- * point is reached again. print the loop and end program.
+ * Description: Floyd's cycle finding algorithm( 2 pointers
+ * used to loop through list at different rates(tortoise- one
+ * step at a time, hare - 2 steps at a time) finds the loop
+ * position in list.
  *
- * Return: number of nodes in list
- *
+ * Return: loop in given list
  */
 
-size_t print_listint_safe(const listint_t *head)
+const listint_t *loop_in_list(const listint_t *head)
 {
-	size_t count = 0;
 	const listint_t *tortoise, *hare, *loop_in_list;
-	int loop_reached = 0;
 
 	if (head == NULL)
 		exit(98);
@@ -40,13 +37,34 @@ size_t print_listint_safe(const listint_t *head)
 			break;
 		}
 	}
+	return (loop_in_list);
+}
+
+/**
+ * print_listint_safe - prints a list
+ *
+ * @head: given linked list
+ *
+ * Description: loops through list and prints until loop
+ * position is reached again, prints it and terminates program.
+ *
+ * Return: number of nodes in list
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	size_t count = 0;
+	int loop_reached  = 0;
+	const listint_t *loop;
+
+	loop = loop_in_list(head);
 	while (head != NULL)
 	{
 		printf("[%p] %d\n", (void *)head, head->n);
 		count++;
 		head = head->next;
 
-		if (head == loop_in_list)
+		if (head == loop)
 		{
 			if (loop_reached == 0)
 			{
