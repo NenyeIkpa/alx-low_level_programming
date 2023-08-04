5083ee82a7faf17539db25e96f3a5ebe98cf5171
NenyeIkpa <nenyeikpa@gmail.com>
2023-08-04 10:55:33 +0000

    task 1 solution

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,20 +6,13 @@
  *
  * @n: given number
  *
- * Description: get the number of bits in n. starting
- * from the leftmost print each bit's value. using a bitmask
- * extract the i-th bit.
  * Return: binary
  *
  */
 
 void print_binary(unsigned long int n)
 {
-	int num_in_bits, i;
-
-	num_in_bits = sizeof(n) * 2;
-	for (i = num_in_bits - 1; i >= 0 ; i--)
-	{
-		(n >> i) & 1 ? _putchar('1') : _putchar('0');
-	}
+	if (n > 1)
+		print_binary(n >> 1);
+	_putchar((n & 1) + '0');
 }
