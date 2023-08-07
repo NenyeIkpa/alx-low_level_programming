2317ab60142ba19c9d9291f1f83ee61717a9538c
NenyeIkpa <nenyeikpa@gmail.com>
2023-08-07 22:16:01 +0000

    function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fildes = open(filename, O_RDWR | O_TRUNC | O_CREAT, 0600);
+	fildes = open(filename, O_WRONLY | O_TRUNC | O_CREAT, 0600);
 	if (fildes == -1)
 		return (-1);
 	bytes_written = write(fildes, text_content, strlen(text_content));
