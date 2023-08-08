58b7b56f707258224280eaa736a862d2199a4c3e
NenyeIkpa <nenyeikpa@gmail.com>
2023-08-08 00:57:17 +0000

    program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,13 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
+ * cp_file_to_file - copies content of one file to another
+ *
+ * @src_filename: source file
+ * @dest_filename: destination file
+ *
+ * Return: int
  */
 
 int cp_file_to_file(char *src_filename, char *dest_filename)
@@ -17,6 +24,8 @@ int cp_file_to_file(char *src_filename, char *dest_filename)
 	if (fd_src == -1 || bytes_read == -1)
 	{
 		close(fd_src);
+		if (close(fd_src) == -1)
+			return (fd_src);
 		return (98);
 	}
 	fd_dest = open(dest_filename, O_RDWR | O_TRUNC | O_CREAT, 0664);
@@ -25,46 +34,48 @@ int cp_file_to_file(char *src_filename, char *dest_filename)
 	{
 		close(fd_dest);
 		close(fd_src);
+		if (close(fd_dest) == -1)
+			return (fd_dest);
 		return (99);
 	}
 	return (0);
 }
 
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
-
 /**
  * main - check the code
  *
+ * @ac: arg count
+ * @av: list of arguments
+ *
  * Return: Always 0.
  */
+
 int main(int ac, char **av)
 {
 	int n;
 
-    if (ac != 3)
-    {
-        dprintf(2, "Usage: cp file_from file_to\n");
-        exit(97);
-    }
-
-    n = cp_file_to_file(av[1], av[2]);
-    if (n == 98)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", av[1] );
-        exit(98);
-    }
-    else if (n == 99)
-    {
-        dprintf(2, "Error: Can't read from file %s\n", av[2] );
-        exit(99);
-    }
-    /** else if (n == 100)
-    {
-     	dprintf(2, "Error: Can't read from file %s\n", av[2] );
-        exit(99);
-    }
-    */
-    return (0);
+	if (ac != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	n = cp_file_to_file(av[1], av[2]);
+	if (n == 98)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	else if (n == 99)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", av[2]);
+		exit(99);
+	}
+	else if (n == 0 || n == -1)
+		exit(0);
+	else
+	{
+		dprintf(2, "Error: Can't close fd %d\n", n);
+		exit(100);
+	}
+	return (0);
 }
