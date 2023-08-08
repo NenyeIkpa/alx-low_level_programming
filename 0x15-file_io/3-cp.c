705fd882e00b7ea10943f36e38408d066181bdfe
NenyeIkpa <nenyeikpa@gmail.com>
2023-08-08 14:09:51 +0000

    logic update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -42,22 +42,31 @@ int main(int ac, char *av[])
 	}
 	fd_src = open(av[1], O_RDONLY);
 	bytes_read = read(fd_src, buffer, 1024);
-	fd_dest = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	do {
-		if (fd_src == -1 || bytes_read == -1)
-		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
-			exit(98);
-		}
+	fd_dest = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);{
+	if (fd_src == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	if (fd_dest == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
+		exit(99);
+	}
+	while ((bytes_read = read(fd_src, buffer, 1024)) > 0)
+	{
 		bytes_written = write(fd_dest, buffer, bytes_read);
-		if (fd_dest == -1 || bytes_written == -1 || bytes_written != bytes_read)
+		if (bytes_written == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", av[2]);
 			return (99);
 		}
-		bytes_read = read(fd_src, buffer, 1024);
-		fd_dest = open(av[2], O_WRONLY | O_APPEND);
-	} while (bytes_read > 0);
+	}
+	if (bytes_read == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 	close_file(fd_src);
 	close_file(fd_dest);
 	return (0);
