b42b389210582466aeab816aa3337bfcf77761c6
NenyeIkpa <nenyeikpa@gmail.com>
2023-08-08 01:15:22 +0000

    program that copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,79 +2,47 @@
 #include <stdio.h>
 
 /**
- * cp_file_to_file - copies content of one file to another
+ * main - copies content of one file to another
  *
- * @src_filename: source file
- * @dest_filename: destination file
+ * @ac: argument count
+ * @av: list of arguments
  *
  * Return: int
  */
 
-int cp_file_to_file(char *src_filename, char *dest_filename)
+int main(int ac, char *av[])
 {
-	int fd_dest, fd_src;
-	char *buffer;
+	int fd_src, fd_dest;
+	char buffer[1024];
 	int bytes_read, bytes_written;
-
-	buffer = malloc(sizeof(char) * 1024);
-	if (buffer == NULL)
-		return (-1);
-	fd_src = open(src_filename, O_RDONLY);
-	bytes_read = read(fd_src, buffer, 1024);
-	if (fd_src == -1 || bytes_read == -1)
-	{
-		close(fd_src);
-		if (close(fd_src) == -1)
-			return (fd_src);
-		return (98);
-	}
-	fd_dest = open(dest_filename, O_RDWR | O_TRUNC | O_CREAT, 0664);
-	bytes_written = write(fd_dest, buffer, bytes_read);
-	if (fd_dest == -1 || bytes_written == -1 || bytes_written != bytes_read)
-	{
-		close(fd_dest);
-		close(fd_src);
-		if (close(fd_dest) == -1)
-			return (fd_dest);
-		return (99);
-	}
-	return (0);
-}
-
-/**
- * main - check the code
- *
- * @ac: arg count
- * @av: list of arguments
- *
- * Return: Always 0.
- */
-
-int main(int ac, char **av)
-{
-	int n;
 
 	if (ac != 3)
 	{
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	n = cp_file_to_file(av[1], av[2]);
-	if (n == 98)
+	fd_src = open(av[1], O_RDONLY);
+	bytes_read = read(fd_src, buffer, 1024);
+	if (fd_src == -1 || bytes_read == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
 		exit(98);
 	}
-	else if (n == 99)
+	fd_dest = open(av[2], O_RDWR | O_TRUNC | O_CREAT, 0664);
+	bytes_written = write(fd_dest, buffer, bytes_read);
+	if (fd_dest == -1 || bytes_written == -1 || bytes_written != bytes_read)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[2]);
-		exit(99);
+		return (99);
 	}
-	else if (n == 0 || n == -1)
-		exit(0);
-	else
+	if (close(fd_src) == -1)
 	{
-		dprintf(2, "Error: Can't close fd %d\n", n);
+		dprintf(2, "Error: Can't close fd %d\n", fd_src);
+		exit(100);
+	}
+	if (close(fd_dest) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd_dest);
 		exit(100);
 	}
 	return (0);
