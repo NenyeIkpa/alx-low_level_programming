75bfa5893a71cf9ee609d63fb74cb952cedd946d
NenyeIkpa <nenyeikpa@gmail.com>
2023-08-08 01:26:14 +0000

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdio.h>
+#include <errno.h>
 
 /**
  * main - copies content of one file to another
@@ -10,9 +11,55 @@
  * Return: int
  */
 
-int main(int ac, char *av[])
+void print_error_and_exit(int code, const char *message) {
+    dprintf(STDERR_FILENO, "Error: %s\n", message);
+    exit(code);
+}
+
+int main(int argc, char *argv[])
 {
-	int fd_src, fd_dest;
+	const char *file_from, *file_to;
+	int fd_from, fd_to;
+	char buffer[1024];
+	ssize_t bytes_read;
+
+    if (argc != 3) {
+        print_error_and_exit(97, "Usage: cp file_from file_to");
+    }
+    file_from = argv[1];
+    file_to = argv[2];
+
+    fd_from = open(file_from, O_RDONLY);
+    if (fd_from == -1) {
+        print_error_and_exit(98, strerror(errno));
+    }
+
+    fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+    if (fd_to == -1) {
+        print_error_and_exit(99, strerror(errno));
+    }
+    while ((bytes_read = read(fd_from, buffer, 1024)) > 0) {
+        ssize_t bytes_written = write(fd_to, buffer, bytes_read);
+        if (bytes_written == -1) {
+            print_error_and_exit(99, strerror(errno));
+        }
+    }
+
+    if (bytes_read == -1) {
+        print_error_and_exit(98, strerror(errno));
+    }
+
+    if (close(fd_from) == -1) {
+        print_error_and_exit(100, strerror(errno));
+    }
+
+    if (close(fd_to) == -1) {
+        print_error_and_exit(100, strerror(errno));
+    }
+
+    return 0;
+
+	/** int fd_src, fd_dest;
 	char buffer[1024];
 	int bytes_read, bytes_written;
 
@@ -46,4 +93,5 @@ int main(int ac, char *av[])
 		exit(100);
 	}
 	return (0);
+	*/
 }
