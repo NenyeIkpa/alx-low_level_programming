daa0e377d1db3ce559cee859bffe820994016e44
NenyeIkpa <nenyeikpa@gmail.com>
2023-10-07 01:16:24 +0000

    task 3 update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -3,6 +3,7 @@
 /**
  * hash_table_set - adds an element to the hash table
  *
+ * @ht: pointer to hash table
  * @key: key of element to be added
  * @value: value of element to be added
  *
@@ -17,6 +18,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || strcmp(key, "") == 0)
 		return (0);
 	hashcode = key_index((const unsigned char *)key, ht->size);
+	while (ht->array[hashcode] != NULL)
+	{
+		if (strcmp(ht->array[hashcode]->key, key) == 0)
+			hash_table_update(ht, hashcode, value);
+		ht->array[hashcode] = ht->array[hashcode]->next;
+	}
 	node = malloc(sizeof(hash_node_t));
 	if (node == NULL)
 		return (0);
@@ -42,5 +49,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		node->next = ht->array[hashcode];
 		ht->array[hashcode] = node;
 	}
-	return (0);
+	return (1);
+}
+
+/**
+ * hash_table_update - updats the value of n existing key in hash table
+ *
+ * @ht: pointer to hash table
+ * @hashcode: hashcode of given key
+ * @value: new value to be placed in existing key
+ *
+ * Return: 1 on success, 0 on failure
+ */
+
+int hash_table_update(hash_table_t *ht, unsigned long int hashcode,
+		const char *value)
+{
+	ht->array[hashcode]->value = strdup(value);
+	if (ht->array[hashcode]->value == NULL)
+		return (0);
+	return (1);
 }
