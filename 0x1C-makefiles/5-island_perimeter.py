bf3c9212dbf0ee259c4ee01aace83116652dc427
NenyeIkpa <nenyeikpa@gmail.com>
2023-11-23 02:41:19 +0000

    task 5 update

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -4,21 +4,23 @@
 """
 
 def island_perimeter(grid):
-    """ calculates the perimeter of given grid """
-    y = 0
-    x = 0
-    
-    if not grid:
+    """ 
+        calculates the perimeter of given grid
+    """
+    if not grid or not grid[0]:
         return 0
-    for row in range(len(grid) - 1):
-            for col in range(len(grid[row]) - 1):
-                if grid[row][col] == 1:
-                    if grid[row][col - 1] == 0:
-                        y += 1
-                    if grid[row][col + 1] == 0:
-                        y += 1
-                    if grid[row - 1][col] == 0:
-                        x += 1
-                    if grid[row + 1][col] == 0:
-                        x += 1
-    return x + y
+
+    assumed_perimeter = 0
+    connections = 0
+    rows = len(grid)
+    cols = len(grid[0])
+    
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                assumed_perimeter += 4
+                if i > 0 and grid[i-1][j] == 1:
+                    connections += 2
+                if j > 0 and grid[i][j-1] == 1:
+                    connections += 2
+    return assumed_perimeter - connections
