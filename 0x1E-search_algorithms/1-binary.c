a05a2f7e963259486a7752f165bc2533b4d2ed7c
NenyeIkpa <nenyeikpa@gmail.com>
2024-01-16 11:14:22 +0000

    task 1

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,37 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of
+ * integers using the Binary search algorithm
+ *
+ * @array: given array
+ * @size: size of array
+ * @value: value to be searched in array
+ *
+ * Return: int
+*/
+
+int binary_search(int *array, size_t size, int value)
+{
+	size_t i, start, end;
+
+	if (array == NULL)
+		return (-1);
+
+	for (start = 0, end = size - 1; end >= start;)
+	{
+		for (i = start; i < end; i++)
+			printf("%d, ", array[i]);
+		printf("%d\n", array[i]);
+
+		i = start + (end - start) / 2;
+		if (array[i] == value)
+			return (i);
+		if (array[i] > value)
+			end = i - 1;
+		else
+			start = i + 1;
+	}
+
+	return (-1);
+}
